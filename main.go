ff0ed73436a2df6b87733381c1ade0d288479eba
Aditya Suryadi <adityasuryaditya@gmail.com>
2023-01-18 22:06:46 +0700

    feat:add rabbitmq connection

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"go-blog/config"
+	"go-blog/exception"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func NewApp() *fiber.App {
@@ -38,5 +41,53 @@ func main() {
 		// logger.Error("Hello Logging")
 		return c.SendString("Hello, Adit!")
 	})
+
+	// route untuk test rabbitmq
+
+	// koneksi ke rabbitmq
+	connRabbitMq, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	if err != nil {
+		log.Fatalf("%s: %s", "failed to connect rabbitMQ", err)
+	}
+
+	app.Get("/send", func(c *fiber.Ctx) error {
+		if c.Query("msg") == "" {
+			log.Println("Missing 'msq' query parameter")
+		}
+
+		// buka koneksi ke rabbitmq
+		ch, err := connRabbitMq.Channel()
+		exception.PanicIfNeeded(err)
+		defer ch.Close()
+
+		// deklarasi queue yang akan di publish dan di subscribe
+
+		_, err = ch.QueueDeclare(
+			"TestQueue", //nama
+			false,       //durable
+			false,       //delete when unused
+			false,       //exclusive
+			false,       // no-wait
+			nil,         //arguments
+		)
+
+		exception.PanicIfNeeded(err)
+
+		err = ch.Publish(
+			"",
+			"TestQueue",
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(c.Query("msg")),
+			},
+		)
+
+		exception.PanicIfNeeded(err)
+
+		return nil
+	})
+
 	app.Listen(":3001")
 }
